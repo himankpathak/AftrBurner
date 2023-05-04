8a46ba8f7871f6a42a3a0c30740e0ba283828a9a
Himank Pathak <himankpathak@gmail.com>
2023-05-04 05:10:17 -0400

    Added init.py file to run multiple build configs

---
+++ init.py
@@ -0,0 +1,19 @@
+import subprocess
+
+
+def RunConan(build_type):
+    subprocess.run(
+        (
+            "conan",
+            "create",
+            ".",
+            "--build=missing",
+            f"--settings=build_type={build_type}",
+            "-tf=",  # Skip tests for now
+        )
+    )
+
+
+if __name__ == "__main__":
+    RunConan("MinSizeRel")
+    RunConan("Debug")
